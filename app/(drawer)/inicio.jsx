07cd9a9b31bfe7724de2ef7b87a2f54be04c35c6
Manoela Urias <manoela.urias@treetech.com.br>
2025-09-08 09:38:25 -0300

    Adiciona mensagem quando banco esta vazio

--- app/(drawer)/inicio.jsx
+++ app/(drawer)/inicio.jsx
@@ -1,12 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
 
 
 
 export default function Inicio() {
-    
 
     return (
-<>
-inicio
-</>
+        <View style={styles.container}>
+            <Text style={{fontSize: 24, textAlign: 'center', width: 250}}>Você ainda não tem nenhum card</Text>
+        </View>
     );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+})
