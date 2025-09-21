ed85c17d2f3194d09151a8d1f68116433376304b
Manoela Urias <manoela.urias@treetech.com.br>
2025-09-21 19:07:43 -0300

    adiciona estilo

--- app/components/CardWords.jsx
+++ app/components/CardWords.jsx
@@ -1,51 +1,119 @@
 import { Languages } from "lucide-react-native";
-import { StyleSheet, Text, View } from "react-native";
-import BotaoTraduzir from "./botaoTraduzir";
-import Favoritar from "./favoritar";
+import { useState } from "react";
+import { Animated, Pressable, StyleSheet, Text, View } from "react-native";
+import Favoritar from './favoritar';
 
 
-export default function CardWords({ id, favoritar, titulo, traducao, onToggleFavorite }) {
+export default function CardWords({ id, favoritar, titulo, traducao, pontuacao, onToggleFavorite }) {
+    const [flipped, setFlipped] = useState(false);
+    const rotateAnim = useState(new Animated.Value(0))[0];
+
+    const flipCard = () => {
+        Animated.spring(rotateAnim, {
+            toValue: flipped ? 0 : 180,
+            useNativeDriver: true,
+            friction: 8,
+            tension: 10,
+        }).start();
+        setFlipped(!flipped);
+    };
+
+    const frontInterpolate = rotateAnim.interpolate({
+        inputRange: [0, 180],
+        outputRange: ["0deg", "180deg"],
+    });
+
+    const backInterpolate = rotateAnim.interpolate({
+        inputRange: [0, 180],
+        outputRange: ["180deg", "360deg"],
+    });
 
     return (
-        <View style={styles.container}>
-            <View style={styles.favoritar}>
-                <Favoritar
-                    onChange={() => onToggleFavorite(id)}
-                    initialChecked={favoritar}
-                />
+        <Pressable onPress={flipCard}>
+            <View style={styles.flipCard}>
+                <View>{pontuacao}</View>
+                <View style={styles.favoritar}>
+                    <Favoritar
+                        onChange={() => onToggleFavorite(id)}
+                        initialChecked={favoritar}
+                    />
+                </View>
+                <Animated.View
+                    style={[
+                        styles.flipCardInner,
+                        { transform: [{ rotateY: frontInterpolate }] },
+                    ]}
+                >
+                    <View style={styles.flipCardFront}>
+                        <Languages style={{ width: 90, height: 90 }} />
+                        <Text style={styles.title}>{titulo}</Text>
+                    </View>
+                </Animated.View>
+                <Animated.View
+                    style={[
+                        styles.flipCardInner,
+                        styles.flipCardBackWrapper,
+                        { transform: [{ rotateY: backInterpolate }] },
+                    ]}
+                >
+                    <View style={styles.flipCardBack}>
+                        <Text style={styles.title}>{traducao}</Text>
+                    </View>
+                </Animated.View>
             </View>
-
-            <Languages style={{ width: 90, height: 90 }} />
-
-            <View style={{ gap: 25, marginTop: 25 }}>
-                <Text style={styles.text}>{titulo}</Text>
-                <BotaoTraduzir />
-            </View>
-        </View>
+        </Pressable>
     );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
+    flipCard: {
+        perspective: 1000,
         flexDirection: 'column',
         borderWidth: 2,
         borderRadius: 14,
         height: 500,
         width: 320,
+
+    },
+    flipCardInner: {
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        backfaceVisibility: "hidden",
+    },
+    flipCardBackWrapper: {
+        top: 0,
+    },
+    flipCardFront: {
+        flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        gap: 10,
-        backgroundColor: '#fff'
+        backgroundColor: "#fff",
+        borderWidth: 1,
+        borderColor: "coral",
+        borderRadius: 16,
+        padding: 10,
+        gap: 40
     },
-    text: {
-        fontSize: 32,
-        textAlign: 'center'
+    flipCardBack: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#566752ff",
+        borderWidth: 1,
+        borderColor: "coral",
+        borderRadius: 16,
+        padding: 10,
+    },
+
+    title: {
+        fontSize: 30
     },
 
     favoritar: {
         position: 'absolute',
         top: 0,
-        left: 0
+        left: 0,
+        zIndex: 300
     }
-})
+});
